e2195345388781106516edb08a18dac02f8435d7
Artur Iablokov <artyapple@gmail.com>
2017-03-22 22:16:27 +0100

    Added: Interface HAWList

--- src/HAWList.java
+++ src/HAWList.java
@@ -1,4 +1,41 @@
 
-public interface HAWList {
+public interface HAWList<T> {
 
+	/**
+	 * inserts the specified element at the specified position in this list.
+	 * @param pos
+	 * @param elem
+	 */
+	public void insert(int pos, T elem);
+
+	/**
+	 * removes the element at the specified position in this list
+	 * @param pos
+	 */
+	public void delete(int pos);
+
+	/**
+	 * removes the element with the specified key in this list
+	 * @param elem
+	 */
+	public void delete();
+
+	/**
+	 * finds the element with the specified key in this list
+	 * @return
+	 */
+	public int find();
+
+	/**
+	 * returns the element at the specified position in this list.
+	 * @param pos
+	 * @return
+	 */
+	public T retrieve(int pos);
+
+	/**
+	 * joins two lists.
+	 * @param list
+	 */
+	public HAWList<T> concat(HAWList<T> list);
 }
